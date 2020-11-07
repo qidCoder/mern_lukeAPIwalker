fd6baf67260917f4e04d746fd1dc80a4688d1cb8
Shelley Ophir <qidcoder@gmail.com>
2020-11-07 13:32:26 -0500

    initial setup - drop down list

---
+++ luke-api-walker/src/App.jsx
@@ -0,0 +1,40 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState } from 'react';
+
+function App() {
+
+  //list of dropdown categories
+  const categories = ['', 'Planets', 'People'];
+
+  //State variables
+  const [category, setCategory] = useState(categories[0]);
+
+
+  return (
+    <div className="App">
+
+      {/* search form */}
+      <form>
+        <label>Search For: </label>
+        <select value={category}>
+
+          {/* map out all the options */}
+          {
+            categories.map( (cat, i) => {
+              return(
+                <option key={i} value={cat}>{cat}</option>
+              )
+            })
+          }
+
+        </select>
+      </form>
+
+    </div>
+  );
+}
+
+export default App;
+
+
